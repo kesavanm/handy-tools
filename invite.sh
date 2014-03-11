2344d55f07866edefabc5f6807be9deb3977c669
Kesavan Muthuvel <hi@kesavan.info>
2014-03-11 10:19:25 -0500

    initial version 0.1

---
+++ invite.sh
@@ -0,0 +1,28 @@
+#!/bin/kish
+
+private love(boy,girl){
+	tour_de_chennai(boy,girl);	
+	until(!mobile_battery)
+		txt(‘blab [...] blab’);
+	for(;;){
+		fight && shout && romance && 
+		smile && tear && talk &&
+		anger && madness;
+	}
+	return (always(♥));	
+}
+
+public wedding(boy,girl,vip,all){
+	@loc = (‘pandiyan_mahal’,’madurai’);
+	boy_knots(@(09:00,10:30),$loc);
+	blessings_by(all);
+	yummy_lunch(*);
+	print ‘♥♥♥ Happy married life ♥♥♥ ’.’\n’
+}
+
+love(boy Kesavan , girl Saranya);
+
+if(date()> 2014-06-01)	# Sunday , Happy Holiday
+
+wedding(Kesavan,Sarnaya,You,World);
+return success;
