bf79aad03e319fbf5b94ac595864b3438e2f5030
Kesavan Muthuvel <hi@kesavan.info>
2014-11-30 17:56:47 -0600

    updated for new version of tree + variabled tmp

--- indexer.sh
+++ indexer.sh
@@ -2,20 +2,26 @@
 # indexer - make a html from dir
 # auth - hi@kesavan.info
 
+TMP='tmp'		# Define Temp Dir
 
 #1 tree the dir
-	tree -CDHFfhi . -o /tmp/_index.html
+	tree -CDHFfh . -o $TMP/_index.html		#No <tr> initially 
 
 #2 beautify
-	awk '{gsub("\&nbsp;\&nbsp;",""); print}' /tmp/_index.html  > /tmp/_index2.html 
+	awk '{gsub("&nbsp;&nbsp;",""); print}' $TMP/_index.html  > $TMP/_index2.html 
 
 #3 add table
 
-	awk  -v T=$TABLE ' {gsub("</h1><p>", "</h1><p> <table><tr><td>  <table STARTXXX><tr>"); print}' /tmp/_index2.html  > /tmp/_index3.html #PSEUDO TABLE
-	awk '{gsub("\<br\>","</td> </tr>\n <tr> <td>"); gsub("\<a","</td><td> <a"); print}' /tmp/_index3.html  > /tmp/_index3.5.html 
+	awk '{gsub("Tree</h1>", "Tree ! </h1> <p> \n <table STARTXXX><tr>"); print}' $TMP/_index2.html > $TMP/_index3.html #PSEUDO TABLE
+	awk '{gsub("<br>","</td> </tr>\n <tr> <td>"); gsub("<a","</td><td> <a"); print}' $TMP/_index3.html  > $TMP/_index3.5.html 
+
+#CMD     tree
+#USER   /(/[\w)\w+ /g
+#SIZE   /\w+]/g
+#FILE	/(]\s)(.)+$/g
 
 #4 close table
-	awk '{gsub("\<tr\> \<td\>\<\/td\> \<\/tr\>", "</table>"); print}' /tmp/_index3.5.html> /tmp/_index4.html	
+	awk '{gsub("<tr> <td></td> </tr>", "</table>"); print}' $TMP/_index3.5.html> $TMP/_index4.html	
 	# <tr> <td></td> </tr>
   
 #5 grep </table> ?
@@ -24,50 +30,52 @@
 	#echo 'grep done'
 	
 # strip all
-	sed -e '/<\/table>/,+50d' /tmp/_index4.html > /tmp/_index5.html
-	TABLE=" class='display' id='books' border='1' cellpadding='0' cellspacing='0' align='left' > <thead> <tr> <th>Information <img src='\/tmp\/.indexer_ki\/sorticon.gif'> <\/th>    <th>File <img src='\/tmp\/.indexer_ki\/sorticon.gif'> <\/th> <\/tr> <\/thead" 	
-	
-	sed "s/STARTXXX/$TABLE/g" < /tmp/_index5.html > /tmp/_index6.html
-
-	head -n -2  /tmp/_index6.html >  /tmp/_index7.html
+	sed -e "/<\/table>/,+50d" $TMP/_index4.html > $TMP/_index5.html
+	TABLE=" class='display' id='books' border='1' cellpadding='0' cellspacing='0' align='left' > 	\
+		<thead> 										\
+			<tr> 										\
+				<th>Information <img src='$TMP\/.indexer_ki\/sorticon.gif'> <\/th> 	\
+				<th>File <img src='$TMP\/.indexer_ki\/sorticon.gif'> <\/th> 		\
+			<\/tr> 										\
+		 <\/thead"
+	sed  "s/STARTXXX/$TABLE/g" < $TMP/_index5.html > $TMP/_index6.html
+	head -n -2  $TMP/_index6.html >  $TMP/_index7.html
 
 # finish it
-	echo " </table>   </td></tr></table>  </body> </html>" >> /tmp/_index7.html		#PSESUDO TABLE
-	tail -n +4 /tmp/_index7.html >  /tmp/_3full.html
+	echo " </table>   </td></tr></table>  </body> </html>" >> $TMP/_index7.html		#PSESUDO TABLE
+	tail -n +4 $TMP/_index7.html >  $TMP/_3full.html
 
-echo ' <!doctype html><html>	<head>' > /tmp/_0.html
+echo 	"<!doctype html> <html> <head>		\
+	 <!-- jQuery -->			\
+		<script type="text/javascript" language="javascript" src="$TMP/.indexer_ki/jquery.js"></script>			\
+		<script type="text/javascript" language="javascript" src="$TMP/.indexer_ki/jquery.dataTables.js"></script> 	\
+	"	> $TMP/_0.html
 
 
 echo "Checks for supporting files"
-if [ -f /tmp/.indexer_ki/jquery.js  -a -f /tmp/.indexer_ki/jquery.dataTables.js  -a -f /tmp/.indexer_ki/indexer.js -a -f /tmp/.indexer_ki/sorticon.gif  ] 
+if [ -f $TMP/.indexer_ki/jquery.js  -a -f $TMP/.indexer_ki/jquery.dataTables.js  -a -f $TMP/.indexer_ki/indexer.js -a -f $TMP/.indexer_ki/sorticon.gif  ] 
   then
 	echo Cache avail 
 else 
 	echo "Getting support files [First time init]"
-	mkdir -p /tmp/.indexer_ki/log/
-	wget -O /tmp/.indexer_ki/jquery.js 		-o /tmp/.indexer_ki/log/jq.log 		"http://kesavan.info/jquery/jquery.js"
-	wget -O /tmp/.indexer_ki/jquery.dataTables.js 	-o /tmp/.indexer_ki/log/jq.dt.log 	"http://kesavan.info/jquery/dataTable/jquery.dataTables.js"
-	wget -O /tmp/.indexer_ki/indexer.js 		-o /tmp/.indexer_ki/log/jq.index.log	"http://kesavan.info/jquery/jq.indexer.js"
-	wget -O /tmp/.indexer_ki/sorticon.gif		-o /tmp/.indexer_ki/log/img.log		"http://kesavan.info/jquery/sorticon.gif"
+	mkdir -p $TMP/.indexer_ki/log/
+	wget -O $TMP/.indexer_ki/jquery.js 		-o $TMP/.indexer_ki/log/jq.log 		"http://kesavan.info/jquery/jquery.js"
+	wget -O $TMP/.indexer_ki/jquery.dataTables.js 	-o $TMP/.indexer_ki/log/jq.dt.log 	"http://kesavan.info/jquery/dataTable/jquery.dataTables.js"
+	wget -O $TMP/.indexer_ki/indexer.js 		-o $TMP/.indexer_ki/log/jq.index.log	"http://kesavan.info/jquery/jq.indexer.js"
+	wget -O $TMP/.indexer_ki/sorticon.gif		-o $TMP/.indexer_ki/log/img.log		"http://kesavan.info/jquery/sorticon.gif"
 fi
 	
-cat  /tmp/_0.html /tmp/.indexer_ki/indexer.js  /tmp/_3full.html  >> /tmp/_index.new.html
+cat  $TMP/_0.html $TMP/.indexer_ki/indexer.js  $TMP/_3full.html  >> $TMP/_index.new.html
 
-grep -v 'href="."' /tmp/_index.new.html > /tmp/_index.new2.html
-awk '{gsub("\<\/thead\>\<tr\>", "</thead>"); print}'  /tmp/_index.new2.html >  indexer.htm
+grep -v 'href="."' $TMP/_index.new.html > $TMP/_index.new2.html
+awk '{gsub("</thead><tr>", "</thead>"); print}'  $TMP/_index.new2.html >  indexer.htm
 
 #remove all
-	rm -f /tmp/_*html
+	rm -f $TMP/_*html
 
 echo "indexer.htm generated"	
 exit 0	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
