8e7fe46a5e63b15e1e8d877b66541c8a806b65a6
Kesavan Muthuvel <hi@kesavan.info>
2018-02-11 20:27:36 -0600

    added few java files

---
+++ java/mysql/Main.java
@@ -0,0 +1,8 @@
+
+public class Main extends MySQLAccess  {
+    public static void main(String[] args) throws Exception {
+        MySQLAccess dao = new MySQLAccess();
+        dao.readDataBase();
+    }
+
+}
