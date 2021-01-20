a8640716559399c9c45fda9ff9e56435394e0546
kesavan muthuvel <kesavan.muthuvel@directenergy.com>
2021-01-19 19:01:13 -0600

    formatted error messages

--- php/timeout.php
+++ php/timeout.php
@@ -1,10 +1,19 @@
 <?php
+
+/* Sample SOAP Server
+1 - Place this script in local env where this can be access via https://SERVER_LOCATION/server.php
+2 - Make sure it is accessible with right permissions
+3 - Any printable stuff will end-up with XML fail in client
+*/
+
+#die ("Direct access not premitted");
+
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
 class MyClass {
   public function helloWorld() {
-            return "hello";
+            return "hello world! from server\n";
   }
 
   public function methTimeOut($sleep){
@@ -14,6 +23,7 @@ class MyClass {
 }
 
 try {
+  echo "SOAP Server is running! Call me from a Client";
   $server = new SOAPServer(
     NULL,
     array(
@@ -26,3 +36,4 @@ try {
 catch (SOAPFault $f) {
   print $f->faultstring;
 }
+
